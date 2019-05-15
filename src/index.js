41b2015658699cedebf276eb7491039618922eda
Michael Link <mcharris@unomaha.edu>
2019-05-15 16:41:25 -0500

    added firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase/app";
+
+firebase.initializeApp({
+    "apiKey": "AIzaSyBOpZxh51xbEqQozkD81Q0azjQeLHo0A90",
+    "appId": "1:1092086720542:web:6d1f3f04f790a515",
+    "databaseURL": "https://my-secrets-182718.firebaseio.com",
+    "storageBucket": "my-secrets-182718.appspot.com",
+    "authDomain": "my-secrets-182718.firebaseapp.com",
+    "messagingSenderId": "1092086720542",
+    "projectId": "my-secrets-182718"
+  });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
